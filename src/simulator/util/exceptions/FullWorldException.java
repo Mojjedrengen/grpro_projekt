cdd5c5d4f2417bda7aca95c681d2deafcb6ab6cd
RockRottenSalad <adam19sw@gmail.com>
2024-11-25 15:15:40 +0100

    Forgot to add new exception to previous branch. Resulted in missing exception

---
+++ src/simulator/util/exceptions/FullWorldException.java
@@ -0,0 +1,8 @@
+package simulator.util.exceptions;
+
+public class FullWorldException extends RuntimeException {
+    public FullWorldException(String message) {
+        super(message);
+    }
+}
+
