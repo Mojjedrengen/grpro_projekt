99056683205269a0b220e357cefaa6fc2b4f6685
Kasper Jensen <kfje@itu.dk>
2024-11-26 14:08:59 +0100

    updated new input

---
+++ src/simulator/actors/RabbitTest.java
@@ -0,0 +1,42 @@
+package simulator.actors;
+
+//import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+
+public class RabbitTest {
+    @Test
+    public void testAct() {
+
+    }
+
+    @Test
+    public void testAssignHole() {
+
+    }
+
+    @Test
+    public void testEat() {
+
+    }
+
+    @Test
+    public void testGoHole() {
+
+    }
+
+    @Test
+    public void testHasHole() {
+
+    }
+
+    @Test
+    public void testIsInHole() {
+
+    }
+
+    @Test
+    public void testReproduce() {
+
+    }
+}
