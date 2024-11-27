ac97d52bb23b703138ff325a4432ece594734552
RockRottenSalad <adam19sw@gmail.com>
2024-11-27 17:47:21 +0100

    Functioning wolves; WorldLoader creates WolfPack given multiple wolves on one line; Wolf unit test

--- src/simulator/actors/WolfPack.java
+++ src/simulator/actors/WolfPack.java
@@ -3,7 +3,10 @@ package simulator.actors;
 import java.util.Set;
 import java.util.HashSet;
 
+import itumulator.world.World;
+
 import simulator.objects.holes.WolfHole;
+import simulator.util.Utilities;
 
 public class WolfPack {
 
@@ -11,8 +14,14 @@ public class WolfPack {
     protected WolfHole assignedHole;
 
     public WolfPack(WolfHole assignedHole) {
+        super();
         this.members = new HashSet<>();
         this.assignedHole = assignedHole;
+    }
+
+    public WolfPack(World world) {
+        this(new WolfHole());
+        world.setTile(Utilities.getRandomEmptyNonBlockingLocation(world, world.getSize()), this.assignedHole);
     }
 
     public Set<Wolf> getMembers() {
@@ -25,6 +34,13 @@ public class WolfPack {
 
     public void addMember(Wolf wolf) {
         this.members.add(wolf);
+    }
+
+    public boolean removeMember(Wolf wolf) {
+        if(!this.members.contains(wolf)) return false;
+
+        this.members.remove(wolf);
+        return true;
     }
 
 } 
