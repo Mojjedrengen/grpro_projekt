fd1a996f855a55d0aa7047aedc30b2976d68861f
Mojjedrengen <99266094+Mojjedrengen@users.noreply.github.com>
2024-12-02 16:25:21 +0100

    Now with a few JavaDocs

--- src/simulator/actors/cordyceps/InfectedAnimal.java
+++ src/simulator/actors/cordyceps/InfectedAnimal.java
@@ -1,26 +1,33 @@
 package simulator.actors.cordyceps;
 
+import itumulator.executable.DisplayInformation;
+import itumulator.executable.DynamicDisplayInformationProvider;
 import itumulator.world.Location;
 import itumulator.world.World;
 import org.jetbrains.annotations.NotNull;
 import simulator.actors.Animal;
+import simulator.actors.Bear;
 import simulator.actors.Rabbit;
+import simulator.actors.Wolf;
 
+import java.awt.*;
 import java.util.Random;
 import java.util.Set;
 
 
-public class InfectedAnimal<T extends Animal> extends Animal implements Cordyceps {
-    //static DisplayInformation largeRabbit = new DisplayInformation(Color.magenta, "rabbit-large-fungi");
-    //static DisplayInformation smallRabbit = new DisplayInformation(Color.magenta, "rabbit-fungi-small");
+public class InfectedAnimal<T extends Animal> extends Animal implements Cordyceps, DynamicDisplayInformationProvider {
+    static DisplayInformation largeRabbit = new DisplayInformation(Color.magenta, "rabbit-large-fungi");
+    static DisplayInformation smallRabbit = new DisplayInformation(Color.magenta, "rabbit-fungi-small");
+    static final DisplayInformation adultBear = new DisplayInformation(Color.magenta, "bear-fungi");
+    static final DisplayInformation babyBear = new DisplayInformation(Color.magenta, "bear-small-fungi");
+    static final DisplayInformation adultWolf = new DisplayInformation(Color.magenta, "wolf-fungi");
+    static final DisplayInformation babyWolf = new DisplayInformation(Color.magenta, "wolf-fungi-small");
 
     private final Class<? extends Animal> hostKind;
     private final Class<T> t;
-    public Location intitLocation;
 
     public InfectedAnimal(Class<T> hostKind, @NotNull World world, Animal host) {
-        super(100, 10 , hostKind, 5);
-        intitLocation = world.getLocation(host);
+        super(100, 100 , hostKind, 5);
         world.delete(host);
         this.age = host.getAge();
         this.hostKind = hostKind;
@@ -35,8 +42,9 @@ public class InfectedAnimal<T extends Animal> extends Animal implements Cordycep
         Random rand = new Random();
         for (T animal : nearbyKind) {
             if (rand.nextInt(101) > 80) {
+                Location l = world.getLocation(animal);
                 InfectedAnimal<? extends Animal> newInfected = new InfectedAnimal<>(hostKind, world, animal);
-                world.setTile(newInfected.intitLocation, newInfected);
+                world.setTile(l, newInfected);
             }
         }
     }
@@ -45,6 +53,7 @@ public class InfectedAnimal<T extends Animal> extends Animal implements Cordycep
     public void decompose(@NotNull World world) {
         if (world.getCurrentTime() == World.getTotalDayDuration()/2) {
             this.takeDamage(1, world);
+            this.age++;
         }
     }
 
@@ -66,5 +75,21 @@ public class InfectedAnimal<T extends Animal> extends Animal implements Cordycep
         }
 
         decompose(world);
+    }
+
+    @Override
+    public DisplayInformation getInformation() {
+        if (hostKind == Rabbit.class) {
+            if (age > 2) return InfectedAnimal.largeRabbit;
+            return InfectedAnimal.smallRabbit;
+        } else if (hostKind == Bear.class) {
+            if (this.age > 10) return InfectedAnimal.adultBear;
+            return InfectedAnimal.babyBear;
+        } else if (hostKind == Wolf.class) {
+            if(this.age > 3) return InfectedAnimal.adultWolf;
+            return InfectedAnimal.babyWolf;
+        } else {
+            return new DisplayInformation(Color.magenta, "fungi");
+        }
     }
 }
